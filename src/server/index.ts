cd4ccf8e1da32110d3bcc1428d5b7a19234621a8
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2023-04-04 14:08:55 -0400

    feat: basic pushapi setup happening

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 dotenv.config();
 import { sequelize } from "./datasource";
+import webpush from "web-push";
 
 import session from 'express-session';
 import cors from "cors"
@@ -36,9 +37,21 @@ app.use("/api/appointments", appointmentRouter);
 app.use("/api/sessions", sessionRouter);
 app.use("/api/mail", mailRouter);
 
-app.get("/api/hello", (req, res) => {
-  console.log("Hello from the server!");
-  res.json({ message: "Hello from the server!" });
+webpush.setVapidDetails(
+  `mailto:${process.env.VAPID_SENDTO}`,
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+);
+
+// Subscribe route to test rn
+// TODO: Move to own file
+// TODO: Setup service worker
+// TODO: Setup push notifications from client side, i.e. ask for permission
+app.post("/api/subscribe", (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({ title: "Push Test" });
+  webpush.sendNotification(subscription, payload).catch((err) => console.error(err));
 });
 
 app.listen(3001, () => {
