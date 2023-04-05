185b7425895e73db198c053889156e6901c8f754
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2023-04-05 18:45:00 -0400

    feat: added docker compose and modified apiService for deployment

--- src/server/index.ts
+++ src/server/index.ts
@@ -16,39 +16,28 @@ export const app = express();
 app.use(bodyParser.json());
 
 const whiteList = ['http://localhost:3000', process.env.PROD_FRONTEND_URL, process.env.PROD_BACKEND_URL, "https://api.scholarly-c09.tech"]
-var corsOptionsDelegate = function (req, callback) {
-	  var corsOptions;
-	    if (whiteList.indexOf(req.header('Origin')) !== -1) {
-		        corsOptions = { origin: true, credentials:true } // reflect (enable) the requested origin in the CORS response
-	    } else {
-		corsOptions = { origin: false } // disable CORS for this request
-		}
-		callback(null, corsOptions) // callback expects two parameters: error and options
-	}
-const corsOptions = {
-origin: (origin, callback) => {
-	if (whiteList.indexOf(origin) !== -1) {
-		callback(null, true)
-	} else {
-		console.log(origin);
-		callback(new Error(`Origin not in list, ${origin}`))
-	}
-},
-credentials: true
+const corsOptionsDelegate = function(req: { header: (arg0: string) => string; }, callback: (arg0: any, arg1: { origin: boolean; credentials?: boolean; }) => void): void {
+    var corsOptions: { origin: boolean; credentials?: boolean; };
+    if (whiteList.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true, credentials: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
 }
 app.use(cors(corsOptionsDelegate));
 
-app.use(cors({origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 
 (async () => {
-  await sequelize.sync({ alter: { drop: false } });
-  console.log("Connected to database!");
+    await sequelize.sync({ alter: { drop: false } });
+    console.log("Connected to database!");
 })();
 
 app.use(session({
-  secret: 'test',
-  resave: false,
-  saveUninitialized: true
+    secret: 'test',
+    resave: false,
+    saveUninitialized: true
 }));
 
 app.use("/api/users", userRouter);
@@ -57,10 +46,10 @@ app.use("/api/sessions", sessionRouter);
 app.use("/api/mail", mailRouter);
 
 app.get("/api/hello", (req, res) => {
-  console.log("Hello from the server!");
-  res.json({ message: "Hello from the server!" });
+    console.log("Hello from the server!");
+    res.json({ message: "Hello from the server!" });
 });
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+    console.log("Server is running on port 3001");
 });
