b851ef33647b6dbf25d086da23e97ce14e21a1d0
maazh10 <maaz.hashmi@mail.utoronto.ca>
2023-04-05 19:31:08 -0400

    fix: navbar styling and footer

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -4,20 +4,29 @@ import styles from "../styles/components/footer.module.scss";
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <p>Footer</p>
-    <a href="https://www.flaticon.com/free-icons/user" title="user icons">
-      User icons created by Flat Icons - Flaticon
-    </a>
-    <a
-      target="_blank"
-      href="https://icons8.com/icon/CNS2xX2b_Fe1/call-disconnected"
-    >
-      Call Disconnected
-    </a>{" "}
-    icon by{" "}
-    <a target="_blank" href="https://icons8.com">
-      Icons8
-    </a>
+    <div className={styles.left}>
+      <img src="/logo.png" alt="logo" />
+      <div className={styles.logoText}>
+        <h1>Scholarly.</h1>
+        <p>© 2023 Scholarly. All rights reserved.</p>
+      </div>
+    </div>
+    <div className={styles.right}>
+      <h3>Credits</h3>
+      <a href="https://www.flaticon.com/free-icons/user" title="user icons">
+        User icons created by Flat Icons - Flaticon
+      </a>
+      <a
+        target="_blank"
+        href="https://icons8.com/icon/CNS2xX2b_Fe1/call-disconnected"
+      >
+        Call Disconnected
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+    </div>
   </footer>
 );
 
